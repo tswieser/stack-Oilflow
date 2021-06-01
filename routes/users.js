694b10226b17322e489bbe03d03696bdbf3c3aca
Joubin T <jubin.taghdir@gmail.com>
2021-06-01 14:53:33 -0700

    signup functionality

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const app = require('../app.js')
+const { User } = require('../db/models')
+const csrf = require('csurf');
+const bcrypt = require('bcryptjs');
 
+const csrfProtection = csrf({cookie: true});
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -9,13 +13,27 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/new', async(req, res) => {
-
-
-
-
+router.get('/new', csrfProtection, async(req, res) => {
+  res.render("new-user", {
+    csrfToken: req.csrfToken()
+  });
 })
 
+router.post('/new', csrfProtection, asyncHandler(async(req, res, next) => {
+  const { user_name, email, first_name, last_name, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.create({
+    user_name,
+    email,
+    first_name,
+    last_name,
+    hashed_password : hashedPassword
+  })
+
+  res.redirect("/");
+  
+}))
 
 
 module.exports = router;
