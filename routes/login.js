a5f40de66af77166e4f54ba36513c7cddd906823
David Mejia <david7mejia@gmail.com>
2021-06-01 18:52:15 -0400

    finish login error valid.

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,47 @@
 const express = require("express");
 const loginRouter = express.Router();
-
+const { asyncHandler, csrfProtection } = require('./utils')
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 
 loginRouter.get("/", (req, res, next) => {
     res.render("login")
 });
 
-// const loginValidators = [
-//     check("email")
-//         .exists({ checkFalsy: true })
-//         .withMessage("You must enter an email address."),
-//     check("password")
-//         .exists({ checkFalsy: true })
-//         .withMessage("You must enter a password."),
-// ];
+const loginValidators = [
+    check("email")
+        .exists({ checkFalsy: true })
+        .withMessage("You must enter an email address."),
+    check("password")
+        .exists({ checkFalsy: true })
+        .withMessage("You must enter a password."),
+];
 
-
-
-loginRouter.post("/", (req, res, next) => {
+loginRouter.post("/", loginValidators, csrfProtection, asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
-    const user = User.findOne({
-        where: { email }
-    })
+    const validationErrors = validationResult(req);
+    const errors = [];
 
 
+    if (validationErrors.isEmpty()) {
+        const user = await User.findOne({
+            where: { email }
+        })
+        if (user) {
+            const passMatch = await bcrypt.compare(password, user.hashed_password.toString());
+            if (passMatch) {
+                //to do login
+                res.redirect('/')
+            } else {
+                errors.push('Login password/email combination is not valid.')
+            }
+        }
+    } else {
+        res.render('login', { csrfToken: req.csrfToken(), errors });
+    }
 
-});
+}));
 
 
 
