6e87765cf14a40bc3a1e11dd75158ab376a9368d
crisMtech95 <cmg.95.col@hotmail.com>
2021-06-01 18:01:33 -0400

    users-loiginbasics

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,25 @@ const loginRouter = express.Router();
 
 
 
-loginRouter.get("/login", (req, res, next) => {
-    res.send("Hi")
+loginRouter.get("/", (req, res, next) => {
+    res.render("login")
+});
+
+const loginValidators = [
+    check("email")
+        .exists({ checkFalsy: true})
+        .withMessage("You must enter an email address."),
+    check("password")
+        .exists({ checkFalsy: true})
+        .withMessage("You must enter a password."),
+];
+
+
+
+loginRouter.post("/", (req, res, next) => {
+    const { email, password} = req.body
+
+    res.redirect("/")
 });
 
 
