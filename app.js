843b56bd3ef51c44cf7d16762801243ddddfbf87
Timothy Wieser <timothyswieser@gmail.com>
2021-06-01 16:48:57 -0400

    create signup pug

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { sequelize } = require('./db/models');
+const { sequelize, User } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
 
 const app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const app = require('../app.js')
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.get('/new', async(req, res) => {
+
+
+
+
+})
+
+
 
 module.exports = router;
