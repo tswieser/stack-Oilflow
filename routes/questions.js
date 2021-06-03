a7dc905e6e10e23d5f4ba8492a3d9e99951a206b
Timothy Wieser <timothyswieser@gmail.com>
2021-06-03 18:42:08 -0400

    debugged the vote counter

--- routes/questions.js
+++ routes/questions.js
@@ -4,13 +4,16 @@ const { User, Question, Answer, Question_like } = require('../db/models');
 const { csrfProtection, asyncHandler, check, validationResult } = require('./utils');
 const { requireAuth } = require('../auth');
 
-class QuestionObject{
-    constructor(id ,title, body, answers, votes){
-        this.title = title,
-        this.body = body,
-        this.answers = answers,
-        this.votes = votes
-    }        
+
+let objArr = []
+class QuestionObject {
+    constructor(id, title, body, answers, votes) {
+        this.id = id,
+            this.title = title,
+            this.body = body,
+            this.answers = answers,
+            this.votes = votes
+    }
 }
 
 
@@ -27,22 +30,35 @@ const questionValidator = [
 
 ]
 
+
 router.get('/', csrfProtection, asyncHandler(async (req, res) => {
-    const questions = await Question.findAll({ 
-        include: [ Answer, Question_like ]
+    const questions = await Question.findAll({
+        include: [Answer, Question_like]
     });
 
-    const allQuestions = questions.map(question => {
-        return question.dataValues;
-    });
-    
-    const allLikes = allQuestions.map(question => {
-        return question.Question_likes
-    });
+    for (let i = 0; i < questions.length; i++) {
+        let question = questions[i]
+        let questionVotes = questions[i].Question_likes;
+        let count = 0
+
+        for (let j = 0; j < questionVotes.length; j++) {
+            let questionLikes = questionVotes[j].question_votes
+            if (questionLikes === true) count++
+            else if (questionLikes === false) count--
+        }
+
+        let newQuestion = new QuestionObject(question.id, question.question_title, question.question_body, question.Answers, count)
+        objArr.push(newQuestion)
+    }
+
+    //console.log(objArr);
+    //console.log(questions[0].Question_likes[0].question_votes);
+    //console.log(questionLikes[0].question_votes)
 
     res.render('questions', {
         // title: 'Questions',
-        questions
+      //  questions,
+        objArr
     });
 }))
 
@@ -81,16 +97,16 @@ router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, r
     const questionsId = parseInt(req.params.id, 10)
     const question = await Question.findByPk(questionsId)
 
-router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-    const questionsId = parseInt(req.params.id, 10)
-    const question = await Question.findByPk(questionsId)
+    router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
+        const questionsId = parseInt(req.params.id, 10)
+        const question = await Question.findByPk(questionsId)
 
-    res.render('question-id', {
-        title: 'Question',
-        question,
-        csrfToken: req.csrfToken()
-    });
-}))
+        res.render('question-id', {
+            title: 'Question',
+            question,
+            csrfToken: req.csrfToken()
+        });
+    }))
 
     res.render('questions-id', {
         question,
