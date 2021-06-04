75e7b360228e783bfd3796a1876756d792c926b7
crisMtech95 <cmg.95.col@hotmail.com>
2021-06-04 12:25:22 -0400

    some styling

--- public/javascripts/answer-ajax.js
+++ public/javascripts/answer-ajax.js
@@ -1,6 +1,8 @@
 //add event listener to the form submit btn
 //prevent default inside of event listener
 
+// const { response } = require("express");
+
 
 // start with fetch submit answer
 //await the fetch (submit req with method post)
@@ -12,16 +14,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const answerForm = document.getElementById('answer-form');
     answerForm.addEventListener('submit', async (e) => {
-        // e.preventDefault()
-        // console.log(`THIS IS THE E TARGET`, e.target.questions_id)
+        // console.log("THIS IS THE WINDOW PATH NAME::::",window.location.pathname)
+        e.preventDefault()
+        console.log(`THIS IS THE E TARGET`, e.target.value)
+        debugger
+
         const reqs = await fetch(window.location.pathname, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(answer_body)
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({answer_body: e.target})
         })
-        const ress = reqs.json()
-        console.log()
+        const ress = await reqs.json()
+        console.log(ress)
+        debugger
     })
 })
